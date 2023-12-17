0b124556bd946dedbcd545290a783eec497270e3
emabondi <emabond69@gmail.com>
2023-12-17 21:22:38 +0100

    fix

--- Piscine/Python-03/ex00/point.py
+++ Piscine/Python-03/ex00/point.py
@@ -4,12 +4,12 @@ class Point:
 		self.y = y
 
 if __name__=="__main__":
-	in1 = input('Insert the coordinates of the first point: ')
-	in1 = in1.split(',', 1)
-	in2 = input('Insert the coordinates of the second point: ')
-	in2 = in2.split(',', 1)
 	try:
+		in1 = input('Insert the coordinates of the first point: ')
+		in1 = in1.split(',', 1)
 		a = Point(float(in1[0]), float(in1[1]))
+		in2 = input('Insert the coordinates of the second point: ')
+		in2 = in2.split(',', 1)
 		b = Point(float(in2[0]), float(in2[1]))
 		print('Their distance is: ', eval(f'(({a.x} - {b.x})**2 + ({a.y} - {b.y})**2)**0.5'))
 	except ValueError:
